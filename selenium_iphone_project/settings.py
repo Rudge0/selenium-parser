f791aeafd27cc1cacb0b0ff042483569e7d0e829
Rudge0 <maksim.pariychuk07@gmail.com>
2025-12-03 00:20:55 +0100

    fdas

--- selenium_iphone_project/settings.py
+++ selenium_iphone_project/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,11 +79,11 @@ WSGI_APPLICATION = 'selenium_iphone_project.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "selenium_iphone_project_db",
-        "USER": "postgres",
-        "PASSWORD": "Karavan1806",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
